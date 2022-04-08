e76f191e0316b6c2729119f11aa3e44ac3c50274
kylefjl <3219029834@mail.nwpu.edu.cn>
2022-04-08 23:02:56 +0800

    图片显示转置

--- main.m
+++ main.m
@@ -5,7 +5,6 @@
 %可改进之处 ：
 %1.增加安全距离（安全阈值）参数，
 % 只限学习交流
-% 作者：2016011301 陈阔 B站分享学习心得 账号：废鹅吃柿子 微信15776629544
 %%
 clear all; clc;
 close all;
@@ -14,12 +13,20 @@ addpath('fun','img');
 name = 'map';
 [M_static,W_static] = load_potential_map('map', n);%这个先不用在意 ？
 %%
+figure
+imshow(M_static);
+title("map");
+
 map_edge=bwperim((M_static));%查找边界
 [row,column]=find(map_edge==1);%获取边界
 start_points=[row';column'];%转置
 %%
 %start_pointstaic = start_pointstaics;
 [D_staic,S] = perform_fast_marching(W_static, start_points);% 
+figure
+imshow(D_staic);
+title("D_staic");
+
 U=rescale(D_staic);%归一化
 % figure;
 % subplot(2,1,1);
@@ -43,9 +50,8 @@ end_points = [490,10];
 end_points = end_points';
 paths = {};
 i=1;
-% figure;
-% D_staic2(D_staic2==Inf) = max(max(D_staic2(D_staic2~=Inf)));
-% imshow(rescale(D_staic2));title("D_staic2");
+
+
 
 paths{i} = compute_geodesic(D_staic2,end_points(:,i));
 if length(paths{i}(:))==2
@@ -53,20 +59,27 @@ if length(paths{i}(:))==2
 end
 ms = 30; lw = 3;
 A = convert_distance_color(D_staic2);
+figure;
+subplot(2,1,1);
+imageplot(A); title("imageplot ")
+subplot(2,1,2);
+imshow(A);title("imshow ")
 
-figure(2)
-clf; hold on;%清除图窗 
-imageplot(A); axis image; axis off;
+figure
+clf; %清除图窗 
+imageplot(A); title("distance_color");
+axis image; axis off;
+hold on;
 h = plot( paths{i}(2,:), paths{i}(1,:), 'k' );
 set(h, 'LineWidth', lw);
 path{1}=paths;
 
 saveas(gcf, [ 'result\',name '-geodesics.png'], 'png');
 [x,y]=meshgrid(1:n,1:n);
-figure(3);
+figure
 Z=D_staic2;
 %Z(W_static==0.001)=0;
-mesh(x,y,Z);
+mesh(x,y,A(:,:,1)');
 %% 加载图片
 function [M,W] = load_potential_map(name, n, options)
 options.null = 0;
